873764d92c698440ed9b4974a9a647d2e7afeed2
rlaehgns69 <rlaehgns69@naver.com>
2024-02-26 15:53:06 +0900

    rc progress REACT_APP붙이기 env

--- emotion_study/src/pages/ImageEx/ImageEx.jsx
+++ emotion_study/src/pages/ImageEx/ImageEx.jsx
@@ -1,6 +1,10 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { ref, uploadBytesResumable } from "firebase/storage";
 import { useRef, useState } from "react";
+import { storage } from "../../configs/firebase/fireBaseConfig";
+import { upload } from "@testing-library/user-event/dist/upload";
+import { Line } from "rc-progress";
 
 const layout = css`
   display: flex;
@@ -18,13 +22,15 @@ function ImageEx(props) {
   const handleButtonClick = () => {
     imgFileRef.current.click();
   };
-
+  const [ uploadFiles, setUploadFiles ] = useState([]);
   const [ previews, setPreviews ] = useState([
-
+  
   ]); // id: 데이터: 임시키값 index 배열에서
+  const [ progressPercent, setProgressPercent ] = useState(0);
   const imgFileRef = useRef();
 
   const handleImgFileChange = (e) => {
+    
       // console.log(e.target.files);//가지고  for안에 자체 Promise(반복)
 
       // let ps = [
@@ -35,7 +41,15 @@ function ImageEx(props) {
 
       // Promise.all(ps).then(result => console.log(result));
 
+      // setUploadFiles([...upload, e.target.files]);
+      
       const files = Array.from(e.target.files);
+
+      if(files.length === 0) {
+        imgFileRef.current.value = "";
+        return;
+      }// 파일 열기 취소
+      setUploadFiles(files);
       let promises = [];
       // Promise new Promise 하자마자 실행 - 정의만
 
@@ -49,12 +63,14 @@ function ImageEx(props) {
         };//정의만
         fileReader.readAsDataURL(file);
       }));
+
       Promise.all(promises)
       .then(result => {
         setPreviews(result);
         console.log(result);
       }); // fileList map사용 불가
     }
+    //미리보기용도 실제로 필요없음. firebase랑 연관 x 저장되는거 아니다.
 
    
 
@@ -86,17 +102,29 @@ function ImageEx(props) {
         // fileReader.readAsDataURL(file);
         // while(fileReader.readyState !== 2) {}
 
+  const handleImageUpload = () => {
+    const file = uploadFiles[0];
+    console.log(uploadFiles);
+    const  storageRef = ref(storage, `files/test/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on();
+  }
 
   
   return (
     <div css={layout}>
       {previews.map((preview, index) =>
-        <div key={index} css={imageLayout}>
-          <img src={preview} alt="" />
-        </div>
+      <>
+          <div key={index} css={imageLayout}>
+             <img src={preview} alt="" />
+          </div>
+          <Line percent={progressPercent} strokeWidth={4} strokeColor={"#22222"}/>
+      </>
       )}
       <input style={{ display: "none" }} type="file" multiple={true} ref={imgFileRef} onChange={handleImgFileChange} />
       <button onClick={handleButtonClick}>이미지 불러오기</button>
+      <button onClick={handleImageUpload}>이미지 업로드</button>
     </div>
   );
       
