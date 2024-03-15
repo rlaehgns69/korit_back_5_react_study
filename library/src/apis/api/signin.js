8e2c682ec960238674227c2dbb345ad39a4dd81c
rlaehgns69 <rlaehgns69@naver.com>
2024-03-15 15:04:52 +0900

    로그인 후 JWT토큰생성 (로그인 성공)

---
+++ library/src/apis/api/signin.js
@@ -0,0 +1,6 @@
+import instance from "../utils/instance";
+
+export const signinRequest = (data) => {
+    const response = instance.post("/auth/signin", data);
+    return response;
+}
--- library/src/pages/SignInPage/SignInPage.jsx
+++ library/src/pages/SignInPage/SignInPage.jsx
@@ -4,16 +4,28 @@ import AuthPageInput from "../../components/AuthPageInput/AuthPageInput";
 import RightTopButton from "../../components/RightTopButton/RightTopButton";
 import { useInput } from "../../hooks/useInput";
 import * as s from "./style";
+import { signinRequest } from "../../apis/api/signin";
 
 function SignInPage() {
   const [ username, usernameChange ] = useInput();
   const [ password, passwordChange ] = useInput();
   // 얘네들은 로그인할때 정규식 체크안함.
+
+  const handleSigninSubmit = () => {
+    signinRequest({
+      username,
+      password
+    }).then(response => {
+      console.log(response);
+    }).catch(error => {
+      console.log(error);
+    })
+  }
   return (
     <>
         <div css={s.header}>
           <h1>로그인</h1>
-          <RightTopButton onClick={null}>로그인하기</RightTopButton>
+          <RightTopButton onClick={handleSigninSubmit}>로그인하기</RightTopButton>
         </div>
         <AuthPageInput type={"text"} name={"username"} placeholder={"사용자이름"} value={username} onChange={usernameChange} />
         <AuthPageInput type={"password"} name={"password"} placeholder={"비밀번호"} value={password} onChange={passwordChange} />
