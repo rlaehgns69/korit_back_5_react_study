9daeba60af47738c9d9cee0ff00fbbb7b89fecf4
rlaehgns69 <rlaehgns69@naver.com>
2024-03-15 10:04:48 +0900

    javascript 정규식 프론트유효성 검사 regex.js userInput.js SignUpPage.jsx

--- library/src/hooks/useInput.js
+++ library/src/hooks/useInput.js
@@ -1,11 +1,49 @@
-import { useState } from "react";
-
-export const useInput = () => {
+import { useEffect, useState } from "react";
+import { REGEX } from "../constants/regex";
+// 외부에서 키값 넘겨줌. userName(username) ...
+export const useInput = (property) => {
   const [ value, setValue ] = useState("");
+  const [ message, setMessage ] = useState(null); 
+
+  // handleOnChange할때마다(value바뀔때마다)
+  useEffect(() => {
+    // if(value.length === 0) {
+
+    // }
+    if(!value) {
+      setMessage(() => null);
+      return;
+    }// 입력한 값이 비어있으면 메시지도 null 리턴
+  
+
+    const regexEntries = Object.entries(REGEX);
+    for(let [k, v ] of regexEntries) {
+      if(property === k) { // 키값이 동일하면
+        if(v.regexr.test(value)) { // 통과  정규식 검사/ 값
+          setMessage(message => {
+            return {
+              type: "success",
+              text: ""
+            }
+          })
+
+        }else {
+          setMessage(() => {
+            return {
+              type: "error",
+              text: v.text
+            }
+
+        })
+          
+        }
+      }
+    }
+  }, [value])
   
   const handleOnChange = (e) => {
     setValue(() => e.target.value);
   }
 
-  return [ value, setValue, handleOnChange ]
+  return [ value, setValue, message, handleOnChange ]
 } 
--- library/src/pages/SignUpPage/SignUpPage.jsx
+++ library/src/pages/SignUpPage/SignUpPage.jsx
@@ -10,151 +10,28 @@ import { useNavigate } from "react-router-dom";
 function SignUpPage() {
   const navigate = useNavigate();
 
-  const [ username, setUsername, userNameChange] = useInput();
-  const [ password, setPassword, passwordChange] = useInput();
-  const [ checkPassword, setCheckPassword, checkPasswordChange] = useInput();
-  const [ name, setName, nameChange] = useInput();
-  const [ email, setEmail, emailChange] = useInput();
-  const [ messageGroup, setMessageGroup ] = useState({
-    username: null,
-    password: null,
-    checkPassword:null,
-    name: null,
-    email: null
-  });
-  // const testErrorMessage = {
-  //   type: "success",
-  //   text: "이미 등록된 사용자이름입니다."
-  // }
-
-  const handleCheckPassword = (e) => {
-    if(!!e.target.value) {
-      setMessageGroup(messageGroup => {
-        return {
-          ...messageGroup,
-          checkPassword: { 
-            type: checkPassword === password ? "success" : "error",
-            text: checkPassword === password ? "" : "비밀번호가 서로 일치하지 않습니다."
-          }
-        }
-      })
-    }else {
-      setMessageGroup(messageGroup => {
-        return {
-          ...messageGroup,
-          checkPassword: null
-        }
-      })
-    }
-  }
-  const handleSignupSubmit = () => {
-
-    if(messageGroup?.checkPassword?.type === "error") {
-      alert("가입할 회원의 정보를 다시 확인하세요.");
-      return;
-    }
-    if(!checkPassword) {
-      setMessageGroup(messageGroup => {
-        return {
-          ...messageGroup,
-          checkPassword: {
-            type: "error",
-            text: "비밀번호를 입력하세요" 
-          }
-        }
-      })
-      return;
-    }
-    const signupData = {
-      username,
-      password,
-      checkPassword,
-      name,
-      email
-    }
-
-    signupRequest(signupData); // promise
+  const [ username, setUsername, usernameMessgage, userNameChange] = useInput("username");
+  const [ password, setPassword, passwordMessage, passwordChange] = useInput("password");
+  const [ checkPassword, setCheckPassword, checkPasswordChange] = useInput("checkPassword");
+  const [ name, setName, nameMessage, nameChange] = useInput("name");
+  const [ email, setEmail, emailMessage, emailChange] = useInput("email");
+  
+  const handleSignSubmit = () => {
 
   }
 
-  const signupRequest = async (signupData) => {
-    try{
-      // 그냥 객체 axios JSON으로 변환
-      const response = await axios.post("http://localhost:8080/auth/signup", signupData)
-      //signupRequest(signupData)
-      if(response.data) { // 정상적인 동작이면 안에 data있음 data안에 true있음. response(true) response.data에 true
-        navigate("/auth/signin")
-      }
-      
-    } catch(error) {//요청자체가 잘못(서버가 켜지지않은 경우, 응답 못받은 경우)
-        // console.log(error);
-        const errorMap = error.response.data;
-        const entries = Object.entries(errorMap);
-        // catch이후 초기화 succes(에러있는 것들만 다시)
-        let newMessageGroup = {
-          username: {
-            type: "success",
-            text: "사용할 수 있는 사용자 이름 입니다."
-          },
-          password: {
-            type: "success",
-            text: ""
-          },
-          checkPassword: {
-            type: "success",
-            text: ""
-          },
-          name: {
-            type: "success",
-            text: ""
-          },
-          email: {
-            type: "success",
-            text: ""
-          }
-        };
-        // catch할 때마다 enwMessageGroup을 초기화
-        // [ [k,v] ]
-        for(let [key, value] of entries) {
-            newMessageGroup = {
-                ...newMessageGroup,
-                [key]: {
-                  type: "error",
-                  text: value 
-                }
-            }// 기존 메세지 그룹에다가 객체 key값 새로운 객체
-            // 메세지 객체를 보낼거라서
-            // input에서 error 띄워줄때 message.type, message
-          }// 에러 부분수정
-          if(newMessageGroup.password.type === "error") {
-            newMessageGroup = {
-              ...newMessageGroup,
-              checkPassword: null
-            } // 체크 입력하고나서 확인창 벗어나면 일치하는지 확인
-            //원래 입력하고 tab 컨트롤러 많아짐.
-            setPassword(() => "");
-            setCheckPassword(() => "");
-          }
-          setMessageGroup(() => newMessageGroup);
-          //마지막에 newMessageGroup체크
-        }
-    // axios json 알아서
-    // .then(response => {
-      
-    // }) 
-  }
 
   return (
     <div>
         <div css={s.header}>
           <h1>회원가입</h1>
-          <RightTopButton onClick={handleSignupSubmit}> 가입하기 </RightTopButton>
+          <RightTopButton onClick={handleSignSubmit}> 가입하기 </RightTopButton>
         </div>
-        <AuthPageInput type={"text"} name={"username"} placeholder={"사용자이름"} value={username} onChange={userNameChange} message={messageGroup.username} />
-        <AuthPageInput type={"password"} name={"password"} placeholder={"비밀번호"} value={password} onChange={passwordChange} message={messageGroup.password} />
-        <AuthPageInput type={"password"} name={"checkPassword"} placeholder={"비밀번호확인"} value={checkPassword} onChange={checkPasswordChange} onBlur={handleCheckPassword} message={messageGroup.checkPassword} />
-        <AuthPageInput type={"text"} name={"name"} placeholder={"성명"} value={name} onChange={nameChange} message={messageGroup.name} />
-        <AuthPageInput type={"text"} name={"email"} placeholder={"이메일"} value={email} onChange={emailChange} message={messageGroup.email} />
+        <AuthPageInput type={"text"} name={"username"} placeholder={"사용자이름"} value={username} onChange={userNameChange} message={usernameMessgage} />
+        <AuthPageInput type={"password"} name={"password"} placeholder={"비밀번호"} value={password} onChange={passwordChange} message={passwordMessage} />
+        <AuthPageInput type={"password"} name={"checkPassword"} placeholder={"비밀번호확인"} value={checkPassword} onChange={checkPasswordChange}  message={null} />
+        <AuthPageInput type={"text"} name={"name"} placeholder={"성명"} value={name} onChange={nameChange} message={nameMessage} />
+        <AuthPageInput type={"text"} name={"email"} placeholder={"이메일"} value={email} onChange={emailChange} message={emailMessage} />
         
         
     </div>
